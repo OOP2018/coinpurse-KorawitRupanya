8c94a7da35bae866398613ef61007865956ee8e8
KorawitRupanya <smart1micky268@gmail.com>
2018-01-21 08:48:49 +0700

    almost done except javadoc in the MoneyUtil

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -5,33 +5,61 @@ import java.util.List;
 
 /**
  * Tester class for check the method in the coin class.
+ * 
  * @author Korawit Rupanya
  *
  */
 public class MoneyUtil {
-	
-	public static void main(String[]args) {
+
+	public static void main(String[] args) {
 		List<Coin> coins = new ArrayList<Coin>();
-		coins.add(new Coin(10.0,"Bath"));
-		coins.add(new Coin(5.0,"Bath"));
-		coins.add(new Coin(2.0,"Bath"));
-		coins.add(new Coin(1.0,"Bath"));
-		coins.add(new Coin(1.0,"Yen")); 
-		coins.add(new Coin(1.0,"Rupie"));
-		coins.add(new Coin(0.5,"Bath"));
-		coins.add(new Coin(-0.5,"Yen"));
+		coins.add(new Coin(10.0, "Bath"));
+		coins.add(new Coin(5.0, "Bath"));
+		coins.add(new Coin(2.0, "Bath"));
+		coins.add(new Coin(1.0, "Bath"));
+		coins.add(new Coin(1.0, "Yen"));
+		coins.add(new Coin(1.0, "Rupie"));
+		coins.add(new Coin(0.5, "Bath"));
+		coins.add(new Coin(-0.5, "Bath"));
 		printCoins(coins);
 		System.out.println();
 		sortCoins(coins);
 		printCoins(coins);
 	}
-	public static void printCoins (List<Coin> coins) {
-		for (int i =0;i<coins.size();i++) {
+
+	/**
+	 * Print list of the coins.
+	 * 
+	 * @param coins
+	 *            is the list of coins.
+	 */
+	public static void printCoins(List<Coin> coins) {
+		for (int i = 0; i < coins.size(); i++) {
 			System.out.println(coins.get(i));
 		}
 	}
-	
+
+	/**
+	 * 
+	 * @param coins
+	 */
 	public static void sortCoins(List<Coin> coins) {
 		java.util.Collections.sort(coins);
+	}
+
+/**
+ * 
+ * @param coins
+ * @param currency
+ * @return
+ */
+	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
+		List<Coin> filter = new ArrayList<Coin>();
+		for (Coin coin : coins) {
+			if (coin.getCurrency().equals(currency)) {
+				filter.add(coin);
+			}
+		}
+		return filter;
 	}
 }
